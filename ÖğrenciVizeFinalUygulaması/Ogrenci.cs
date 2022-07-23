a62c0a53d888b19ed3a9dee5a1a15e9be899f299
agcayberkay <berkay_agcay@hotmail.com>
2022-07-23 12:17:56 +0300

    Add project files.

---
+++ ÖğrenciVizeFinalUygulaması/Ogrenci.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ÖğrenciVizeFinalUygulaması
+{
+    public class Ogrenci
+    {
+        private int OgrenciNo;
+
+        private string OgrenciAdı;
+
+        private string OgrenciSoyad;
+
+        private int Vize;
+
+        private int Final;
+
+        private string OkulIsmi;
+
+
+
+
+        public Ogrenci(int _OgrenciNo, string _OgrenciAdı, string _OgrenciSoyad, int _Vize, int _Final, string _OkulIsmi)
+        {
+            OgrenciNo = _OgrenciNo;
+            OgrenciAdı = _OgrenciAdı;
+            OgrenciSoyad = _OgrenciSoyad;
+            Vize = _Vize;
+            Final = _Final;
+            OkulIsmi = _OkulIsmi;
+        }
+
+
+
+
+        public  void ogrenciBilgieriGöster()
+        {
+
+            Console.WriteLine("Ögrenci Numarası:" + OgrenciNo);
+            Console.WriteLine("Ögrenci Adı:" + OgrenciAdı);
+            Console.WriteLine("Ögrenci Soyadı:" + OgrenciSoyad);
+            Console.WriteLine("Ögrenci Vize:" + Vize);
+            Console.WriteLine("Ögrenci Final:" + Final);
+            Console.WriteLine("Okul:" + OkulIsmi);
+            Console.WriteLine("-----------------------------------------------------------------------");
+
+
+        }
+
+        public double ogrenciOrtalaması()
+        {
+            double ortalama = (Vize * 30 / 100) + (Final * 70 / 100);
+            return ortalama;
+            Console.WriteLine("-----------------------------------------------------------------------");
+
+        }
+
+        public void okulGetir()
+        {
+
+            Console.WriteLine("Öğrenci Okul İsmi:" + OkulIsmi);
+            Console.WriteLine("-----------------------------------------------------------------------");
+
+        }
+
+
+    }
+}
---
+++ ÖğrenciVizeFinalUygulaması/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ÖğrenciVizeFinalUygulaması
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Ogrenci ogrenci1 = new Ogrenci(1, "Berkay", "Ağçay", 45, 70, "Ankara Üniversitesi");
+
+            Console.WriteLine("Uygulamamıza hoşgeldiniz... Lütfen yapmak istediğiniz işlemi seçiniz .");
+            Console.WriteLine("-----------------------------------------------------------------------");
+
+            bool kontrol = true;
+
+
+            while (kontrol)
+            {
+                isimleriGöster();
+                Console.WriteLine("-----------------------------------------------------------------------");
+                string secim = Console.ReadLine();
+
+                switch (secim)
+                {
+                    case "1":
+                        ogrenci1.ogrenciBilgieriGöster();
+                        break;
+
+
+                    case "2":
+
+                        double ogrenciOrtalama = ogrenci1.ogrenciOrtalaması();
+                        Console.WriteLine("Öğrencinin Ortalaması :" + ogrenciOrtalama);
+                        break;
+
+                    case "3":
+                        ogrenci1.okulGetir();
+                        break;
+
+                    case "4":
+                        kontrol = false;
+                        break;
+
+
+
+                }
+
+
+            }
+
+
+            static void isimleriGöster()
+            {
+
+                Console.WriteLine("1- Öğrenci Bilgileri");
+                Console.WriteLine("2- Öğrenci Ortalaması");
+                Console.WriteLine("3- Öğrenci Okul İsmi");
+                Console.WriteLine("4- Çıkış Yap");
+            }
+        }
+    }
+}
